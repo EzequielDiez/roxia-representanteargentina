390918d228c061bd3a27a3836480d4923c539039
EzequielDiez <ezequieldiez.emd@gmail.com>
2024-08-23 00:51:38 -0300

    1.0.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,17 @@ module.exports = {
         'custom-green': '#5E8A5A',
         'custom-gray-font': '#282828',
         'custom-red-font': '#C42A2A',
+        'custom-gray-section': '#EFEFEF',
+        'custom-red-card': {
+          DEFAULT: '#FF1D25',
+          '50': 'rgba(255, 29, 37, 0.5)', // 50% de opacidad
+          '30': 'rgba(255, 29, 37, 0.3)', // 30% de opacidad
+          // Puedes agregar más niveles de opacidad si lo necesitas
+        },
+        'custom-blue-card': {
+          DEFAULT: '#1572FF',
+          '30': 'rgba(21, 114, 255, 0.3)',
+        },
       },
       fontFamily: {
         acumin: ["AcuminVariableConcept", "sans-serif"],
