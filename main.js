c9367070132e687d5fab7bfd08708852349661f5
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2024-08-26 14:53:29 -0300

    update

--- main.js
+++ main.js
@@ -18,29 +18,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const esFlagMobile = document.getElementById('es-flag-mobile');
   const enFlagMobile = document.getElementById('en-flag-mobile');
 
+  const updateFlagStyles = (flag, addClasses, removeClasses) => {
+    flag.classList.add(...addClasses);
+    flag.classList.remove(...removeClasses);
+  };
+
   if (currentPath.includes('index_es.html')) {
-    esFlag.classList.add('bg-white', 'text-black');
-    esFlag.classList.remove('text-white');
-    enFlag.classList.remove('bg-white', 'text-black');
-    enFlag.classList.add('text-white');
+    updateFlagStyles(esFlag, ['bg-white', 'text-black'], ['text-white']);
+    updateFlagStyles(enFlag, ['text-white'], ['bg-white', 'text-black']);
 
     if (esFlagMobile && enFlagMobile) {
-      esFlagMobile.classList.add('bg-white', 'text-black');
-      esFlagMobile.classList.remove('text-white');
-      enFlagMobile.classList.remove('bg-white', 'text-black');
-      enFlagMobile.classList.add('text-white');
+      updateFlagStyles(esFlagMobile, ['bg-white', 'text-black'], ['text-white']);
+      updateFlagStyles(enFlagMobile, ['text-white'], ['bg-white', 'text-black']);
     }
   } else if (currentPath.includes('index_en.html')) {
-    enFlag.classList.add('bg-white', 'text-black');
-    enFlag.classList.remove('text-white');
-    esFlag.classList.remove('bg-white', 'text-black');
-    esFlag.classList.add('text-white');
+    updateFlagStyles(enFlag, ['bg-white', 'text-black'], ['text-white']);
+    updateFlagStyles(esFlag, ['text-white'], ['bg-white', 'text-black']);
 
     if (esFlagMobile && enFlagMobile) {
-      enFlagMobile.classList.add('bg-white', 'text-black');
-      enFlagMobile.classList.remove('text-white');
-      esFlagMobile.classList.remove('bg-white', 'text-black');
-      esFlagMobile.classList.add('text-white');
+      updateFlagStyles(enFlagMobile, ['bg-white', 'text-black'], ['text-white']);
+      updateFlagStyles(esFlagMobile, ['text-white'], ['bg-white', 'text-black']);
     }
   }
 
@@ -57,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const navbarLogo = document.getElementById('navbar-logo');
   const inicioScreen = document.getElementById('contentInicio');
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) { // Cambia 50 por el valor que necesites
+    if (window.scrollY > 100) { // Cambia 100 por el valor que necesites
       navbarLogo.classList.add('visible');
       inicioScreen.classList.add('oculto');
     } else {
@@ -66,15 +63,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  document.addEventListener('scroll', function() {
+  document.addEventListener('scroll', function () {
     const section = document.getElementById('first-section');
     const scrollPosition = window.scrollY || window.pageYOffset;
-  
+
     if (scrollPosition >= section.offsetTop) {
       section.style.backgroundAttachment = 'fixed';
     } else {
       section.style.backgroundAttachment = 'scroll';
     }
   });
-  
+
+  // Función para manejar la observación y animación de elementos
+  const handleObserver = (targetElements, animationClasses) => {
+    const observerOptions = {
+      root: null, // La ventana es el viewport
+      rootMargin: '0px',
+      threshold: 1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('opacity-0');
+          entry.target.classList.add('opacity-100', ...animationClasses[entry.target.id]);
+          observer.unobserve(entry.target); // Deja de observar una vez que la animación se activa
+        }
+      });
+    }, observerOptions);
+
+    // Observa los elementos
+    targetElements.forEach(element => observer.observe(element));
+  };
+
+  // Elementos a observar con sus respectivas animaciones
+  const targets = [
+    document.getElementById('boxFirstSection'),
+    document.getElementById('first-client-box'),
+    document.getElementById('second-client-box')
+  ];
+
+  // Clases de animación para cada elemento
+  const animationClasses = {
+    'boxFirstSection': ['animate__animated', 'animate__fadeInUp'],
+    'first-client-box': ['animate__fadeInLeft'],
+    'second-client-box': ['animate__fadeInRight']
+  };
+
+  handleObserver(targets, animationClasses);
+
+  // Inicialización de Swiper
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true,
+  });
 });
